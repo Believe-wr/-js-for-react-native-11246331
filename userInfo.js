9b56603a7227908b1e1b885ccd8514edd56463ce
Believe-wr <bwornor@st.ug.edu.gh>
2024-05-18 07:30:56 -0700

    Task 3

---
+++ userInfo.js
@@ -0,0 +1,12 @@
+// Task 3
+function createUserProfiles(names, modifiedNames) {
+    let id = 1;
+    return names.map((name, index) => {
+      return {
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: id++
+      };
+    });
+  }
+  
